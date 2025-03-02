4a9228e36779356f42878943400ff5e41eae9f85
Huangshuqi <1105374939@qq.com>
2025-03-02 13:07:16 +1100

    Rebuild project arch

--- SegNext/datasets/__init__.py
+++
@@ -1,6 +0,0 @@
-from .cityscapes import CityScapes
-from .mypath import Path
-from .utils import *
-# from .coco import *
-# from .pascal import *
-from .custom_transforms import *
---
+++ datasets/__init__.py
@@ -0,0 +1,6 @@
+from .cityscapes import CityScapes
+from .mypath import Path
+from .utils import *
+# from .coco import *
+# from .pascal import *
+from .custom_transforms import *
